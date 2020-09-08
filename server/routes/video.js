647019beed1211de5a09c107c6824c4ec728ee9a
MJbae <bmj5208@gmail.com>
2020-09-08 11:46:32 +0900

    feat: video upload 기능 중 dropdown 추가

---
+++ server/routes/video.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const router = express.Router();
+// const { Video } = require("../models/Video");
+
+const { auth } = require("../middleware/auth");
+const multer = require("multer");
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "uploads/");
+  },
+  filename: (req, file, cb) => {
+    cb(null, `${Date.now()}_${file.originalname}`);
+  },
+  fileFilter: (req, file, cb) => {
+    const ext = path.extname(file.originalname);
+    if (ext !== ".mp4" || ext !== ".png") {
+      return cb(res.status(400).end("only png, mp4 allowed"), false);
+    }
+    cb(null, true);
+  },
+});
+
+const upload = multer({ storage: storage }).single("file");
+
+//=================================
+//             Video
+//=================================
+
+router.post("/uploadfiles", (req, res) => {
+  upload(req, res, (err) => {
+    if (err) {
+      return res.json({ success: false, err });
+    }
+    return res.json({
+      success: true,
+      filePath: res.req.file.path,
+      fileName: res.req.file.filename,
+    });
+  });
+});
+
+module.exports = router;
