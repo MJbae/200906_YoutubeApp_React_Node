1f26f960a41c6169bc53d07fa9166de5f542d2f7
MJbae <bmj5208@gmail.com>
2020-09-11 09:38:19 +0900

    chore: reply comment 템플릿 추가

--- client/src/components/views/DetailVideoPage/Sections/ReplyComment.js
+++ client/src/components/views/DetailVideoPage/Sections/ReplyComment.js
@@ -1,7 +1,35 @@
 import React from "react";
+import SingleComment from "./SingleComment";
 
 function ReplyComment() {
-  return <div></div>;
+  const renderReplyComment = (parentCommentId) => {
+    props.commentLists.map((comment, index) => {
+      <React.Fragment>
+        {comment.responseTo === parentCommentId && (
+          <div>
+            <SingleComment
+              refreshFunction={props.refreshFunction}
+              comment={comment}
+              postId={props.postId}
+            ></SingleComment>
+            <ReplyComment
+              commentLists={props.commentLists}
+              postId={props.videoId}
+              parentCommentId={comment._id}
+              refreshFunction={props.refreshFunction}
+            ></ReplyComment>
+          </div>
+        )}
+      </React.Fragment>;
+    });
+  };
+
+  return (
+    <div>
+      <p style={{ fontSize: "14px", margin: 0, color: "gray" }}></p>
+      {renderReplyComment(props.parentCommentId)}
+    </div>
+  );
 }
 
 export default ReplyComment;
