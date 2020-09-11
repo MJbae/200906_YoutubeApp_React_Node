d83302e556a8555ea244f52a9a045cfaa8e2698c
MJbae <bmj5208@gmail.com>
2020-09-11 10:07:38 +0900

    feat: reply comment 기능 추가

--- client/src/components/views/DetailVideoPage/Sections/ReplyComment.js
+++ client/src/components/views/DetailVideoPage/Sections/ReplyComment.js
@@ -1,33 +1,57 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import SingleComment from "./SingleComment";
 
-function ReplyComment() {
-  const renderReplyComment = (parentCommentId) => {
-    props.commentLists.map((comment, index) => {
+function ReplyComment(props) {
+  const [ChildCommentNumber, setChildCommentNumber] = useState(0);
+  const [OpenReplyComments, setOpenReplyComments] = useState(false);
+
+  useEffect(() => {
+    let commentNumber = 0;
+    props.CommentLists.map((comment) => {
+      if (comment.responseTo === props.parentCommentId) {
+        commentNumber++;
+      }
+    });
+    setChildCommentNumber(commentNumber);
+  }, [props.CommentLists, props.parentCommentId]);
+
+  let renderReplyComment = (parentCommentId) =>
+    props.CommentLists.map((comment, index) => (
       <React.Fragment>
         {comment.responseTo === parentCommentId && (
-          <div>
+          <div style={{ width: "80%", marginLeft: "40px" }}>
             <SingleComment
-              refreshFunction={props.refreshFunction}
               comment={comment}
               postId={props.postId}
-            ></SingleComment>
-            <ReplyComment
-              commentLists={props.commentLists}
-              postId={props.videoId}
-              parentCommentId={comment._id}
               refreshFunction={props.refreshFunction}
-            ></ReplyComment>
+            />
+            <ReplyComment
+              CommentLists={props.CommentLists}
+              parentCommentId={comment._id}
+              postId={props.postId}
+              refreshFunction={props.refreshFunction}
+            />
           </div>
         )}
-      </React.Fragment>;
-    });
+      </React.Fragment>
+    ));
+
+  const handleChange = () => {
+    setOpenReplyComments(!OpenReplyComments);
   };
 
   return (
     <div>
-      <p style={{ fontSize: "14px", margin: 0, color: "gray" }}></p>
-      {renderReplyComment(props.parentCommentId)}
+      {ChildCommentNumber > 0 && (
+        <p
+          style={{ fontSize: "14px", margin: 0, color: "gray" }}
+          onClick={handleChange}
+        >
+          View {ChildCommentNumber} more comment(s)
+        </p>
+      )}
+
+      {OpenReplyComments && renderReplyComment(props.parentCommentId)}
     </div>
   );
 }
